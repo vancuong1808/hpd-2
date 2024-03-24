5e4146bdf1acb924a446cdf61f4f0dfe015f3e44
Cường Văn <vancuong1808@gmail.com>
2024-03-24 11:10:33 +0700

    new initial

---
+++ asset/main.js
@@ -0,0 +1,7 @@
+let nhac = document.querySelector("#nhac");
+let btn = document.querySelector("#music");
+// let check = false;
+console.log( btn )
+btn.addEventListener('click', function(){
+    nhac.play()
+  })
